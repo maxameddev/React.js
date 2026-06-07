6f26482add90e1a7eb356cfb6787fb40f8738419
maxameddev <engmaxamed40@gmail.com>
2026-06-07 16:33:56 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {Usercard} from "./Greeting";
+
+function App() {
+  return (
+    <Usercard />
+  );
+}
+
+export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+ 
+  export const Usercard = ()=>{
+    return (
+      <div>
+        <h1>Mohamed</h1>
+        <p>Mohamed@example.com</p>
+      </div>
+    )
+  }
+
+
+//  export default Greeting;
