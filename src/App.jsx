28f2f6f1bb3e57628338d932a72c2e7672db3f43
maxameddev <engmaxamed40@gmail.com>
2026-06-17 17:06:42 +0300

    Created login system

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
-import GithubSearch from "./Greeting";
+import { useState } from "react";
+import Greeting from "./Greeting";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (username.trim() !== "") {
+      setIsLoggedIn(true);
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername("");
+  };
+
   return (
     <div>
-      <GithubSearch />
+      {isLoggedIn ? (
+        <Greeting username={username} logout={handleLogout} />
+      ) : (
+        <>
+          <h1>Login</h1>
+
+          <form onSubmit={handleLogin}>
+            <div>
+              <label>Username:</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label>Password:</label>
+              <input type="password" />
+            </div>
+
+            <button type="submit">Login</button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
